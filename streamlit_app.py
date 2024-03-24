d9afa805752d3de550d2d1463b901caf8cfe6f7d
Kipsang Mutai <85893532+kipsangchepesa@users.noreply.github.com>
2024-03-24 20:15:59 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
-#import streamlit as st
-#import numpy as np
-#import cv2
-#from cvzone.HandTrackingModule import HandDetector
-#from cvzone.ClassificationModule import Classifier
-#import math
+import streamlit as st
+import numpy as np
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import math
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
